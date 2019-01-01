4dc2fa1c798e0a4db354e8fbc57eaea17e2ccc11
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-01-01 20:35:42 +0200

    Modify partial() to pass btn and indx to functions connected to buttons

--- GUI.py
+++ GUI.py
@@ -29,21 +29,37 @@ class Window(QtWidgets.QMainWindow):
 		self.Glayout = QtWidgets.QGridLayout()
 		self.Vlayout.addLayout(self.Glayout)
 		# create 9 buttons and add them to the list
+		self.btn_indx = 0 # counter for buttons indeces
 		for row in range(3):
 			for col in range(3):
 				self.btn = QtWidgets.QPushButton("Button"+str(row)+str(col))
 				# connect each button while looping to the function passing the button as argument
-				self.btn.clicked.connect(partial(self.btn_clicked, self.btn))
+				self.btn.clicked.connect(partial(self.btn_clicked, self.btn, self.btn_indx)) # currying, or partial application
+				self.btn_indx += 1
 				self.btn.setMinimumSize(100, 100)
 				self.Glayout.addWidget(self.btn,row,col)
+
 		
+		# add a list to store Xs and Os
+		self.click_list = [0, 1, 2,
+						   3, 4, 5,
+		                   6, 7, 8]
+		self.counter = 0
+
 		for i in range(9):
 			print(self.Glayout.itemAt(i).widget())
 
 	#_______________________________________METHODS______________________________________
 
-	def btn_clicked(self, btn):
+	def btn_clicked(self, btn, indx):
 		print(btn)
+		print(indx)
+		if self.counter % 2 == 0:
+			btn.setText("X")
+			self.counter += 1
+		else:
+			btn.setText("O")
+			self.counter += 1
 
 
 
