05d28fe9c5ade9b74a82cfa83132ea9a7e98861e
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-01-01 15:41:28 +0200

    Improve check() function

--- functonality.py
+++ functonality.py
@@ -22,31 +22,21 @@ def playerO():
 	print()
 	lista[index-1] = " O "
 
+def triplet_check(i, j, k):
+	if lista[i] == lista[j] == lista[k]:
+		print(str(lista[i]) + "won!")
+		sys.exit()
+	else: pass
+
 def check():
-	if lista[0] == lista[1] == lista[2]:
-		print(str(lista[0]) + "won!")
-		sys.exit()
-	if lista[3] == lista[4] == lista[5]:
-		print(str(lista[3]) + "won!")
-		sys.exit()
-	if lista[6] == lista[7] == lista[8]:
-		print(str(lista[6]) + "won!")
-		sys.exit()
-	if lista[0] == lista[3] == lista[6]:
-		print(str(lista[0]) + "won!")
-		sys.exit()
-	if lista[1] == lista[4] == lista[7]:
-		print(str(lista[1]) + "won!")
-		sys.exit()
-	if lista[2] == lista[5] == lista[8]:
-		print(str(lista[2]) + "won!")
-		sys.exit()
-	if lista[0] == lista[4] == lista[8]:
-		print(str(lista[0]) + "won!")
-		sys.exit()
-	if lista[2] == lista[4] == lista[6]:
-		print(str(lista[2]) + "won!")
-		sys.exit()
+	triplet_check(0, 1, 2)
+	triplet_check(3, 4, 5)
+	triplet_check(6, 7, 8)
+	triplet_check(0, 3, 6)
+	triplet_check(1, 4, 7)
+	triplet_check(2, 5, 8)
+	triplet_check(0, 4, 8)
+	triplet_check(2, 4, 6)
 
 print_lista()
 playerX()
