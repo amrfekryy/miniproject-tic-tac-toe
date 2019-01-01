f11f4d7c913a0f38cf726fa5f08d7ff6d31c7310
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-01-01 17:20:57 +0200

    Initialize a window in GUI.py

---
+++ GUI.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5 import QtWidgets
+
+class Window(QtWidgets.QMainWindow):
+
+	def __init__(self):
+		super(Window, self).__init__()
+		
+		self.setGeometry(450, 200, 600, 400)
+		self.setWindowTitle("Tic Tac Toe")
+
+
+
+def main():
+	app = QtWidgets.QApplication(sys.argv)
+	app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
+	window = Window()
+	window.show()
+	sys.exit(app.exec_())
+
+main()
